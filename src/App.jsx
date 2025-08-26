fc020b633f08a93c0299ea5fd23573ae3366aa75
amy-verma <amitjsr8786@gmail.com>
2025-08-26 12:35:56 +0530

    todo

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+const App=()=>{
+  const [add,setAdd]=useState("")
+  const [list,setList]=useState([])
+
+  const handleOnChange=(e)=>{
+    setAdd(e.target.value)
+  }
+
+  const handleAdd=()=>{
+    setList([...list,add])
+    setAdd("")
+  }
+
+  const handleDelete=(i)=>{
+    const updatedList=list.filter((ele,id)=>{
+      return i!=id
+    })
+    setList(updatedList)
+  }
+
+  return (
+  <>
+  <h2>Todo Apps</h2>
+  <input type="text" placeholder='Enter Todo' value={add} onChange={handleOnChange}/>
+  <button onClick={handleAdd}>Add</button>
+  <div>
+    <ul>
+      {
+        list.map((ele,i)=>{
+          return <li key={i}>{ele} <button onClick={()=>handleDelete(i)}>Delete</button></li> 
+        })
+      }
+    </ul>
+  </div>
+  </>
+  )
+}
+
+export default App
