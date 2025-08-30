c32131d015658e17614c101bfd57d1c00e2dfd62
amy-verma <amitjsr8786@gmail.com>
2025-08-30 22:36:05 +0530

    addtocart-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,108 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
 const App=()=>{
+  const [user,setUser]=useState([])
+  const [selected,setSelected]=useState([])
 
-  const [input,setInput]=useState("")
-  const [list,setList]=useState([])
-  
-  const handleInputAdd=(e)=>{
-    console.log(e.target.value);
-    setInput(e.target.value)
-  }
-  const handleAdd=()=>{
-    if(input.trim()===""){
-      return 
+   const FetchData=async()=>{
+      try{
+        const data=await fetch("https://jsonplaceholder.typicode.com/users");
+        const response=await data.json()
+        console.log(response)
+        setUser(response)
+      }catch(error){
+        console.log(error);
+      }
     }
-    const updatedValue=[...list,input]
-    setList(updatedValue)
-   
-    setInput("")
-    console.log(updatedValue)
-  }
 
-  const handleRemove=(index)=>{
-    const updatedList=list.filter((ele,id)=>{
-     return  index!=id
-    })
-    setList(updatedList)
-  }
+    useEffect(()=>{
+      FetchData()
+    },[])
+
+    const handleUserClick=(ele)=>{
+      
+     setSelected((prev)=>{
+      if(prev.some((prevEle)=>ele.id===prevEle.id)){
+        return prev
+      }else{
+        return [...prev,ele]
+      }
+     })
+      }
+    
 
   return (
     <div>
-      <div>
-        <h2>To Do App</h2>
-        <input type="text" value={input} onChange={handleInputAdd}/>
-        <button onClick={handleAdd}> Add</button>
-      </div>
-      <ul>
-        {
-          list.map((ele,index)=>{
-            return <li key={index}>{ele} <button onClick={()=>handleRemove(index)}>Remove</button></li>
-          })
-        }
-      </ul>
+   <ul>
+    {
+      user.map((ele,id)=>{
+        return <li onClick={()=>handleUserClick(ele)} key={ele.id}>{ele.name}</li>
+      })
+    }
+   </ul>
+   <div>
+    <ul>
+      {
+        selected.map((ele)=>{
+          return <li key={ele.id}>{ele.name}</li>
+        })
+      }
+    </ul>
+   </div>
     </div>
   )
 }
+export default App
 
-export default App;
+// import { useState } from "react";
+
+// const App=()=>{
+
+//   const [input,setInput]=useState("")
+//   const [list,setList]=useState([])
+  
+//   const handleInputAdd=(e)=>{
+//     console.log(e.target.value);
+//     setInput(e.target.value)
+//   }
+//   const handleAdd=()=>{
+//     if(input.trim()===""){
+//       return 
+//     }
+//     const updatedValue=[...list,input]
+//     setList(updatedValue)
+   
+//     setInput("")
+//     console.log(updatedValue)
+//   }
+
+//   const handleRemove=(index)=>{
+//     const updatedList=list.filter((ele,id)=>{
+//      return  index!=id
+//     })
+//     setList(updatedList)
+//   }
+
+//   return (
+//     <div>
+//       <div>
+//         <h2>To Do App</h2>
+//         <input type="text" value={input} onChange={handleInputAdd}/>
+//         <button onClick={handleAdd}> Add</button>
+//       </div>
+//       <ul>
+//         {
+//           list.map((ele,index)=>{
+//             return <li key={index}>{ele} <button onClick={()=>handleRemove(index)}>Remove</button></li>
+//           })
+//         }
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
 
 
 // import { useState } from 'react'
